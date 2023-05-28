fc68be8aecf83f8d46e66e709793f6fed376927e
Plamen Petkov <plams.petkov@gmail.com>
2023-05-28 22:03:22 +0200

    Update App 2

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -11,31 +11,31 @@ import Login from './components/Login';
 import { useEffect } from 'react';
 import { fetchUser } from './utils/fetchUser';
 
-// const router = createBrowserRouter([
-// 	{
-// 		path: 'login',
-// 		element: <Login />,
-// 	},
-// 	{
-// 		path: '/*',
-// 		element: <Home />,
-// 	},
-// ]);
+const router = createBrowserRouter([
+	{
+		path: 'login',
+		element: <Login />,
+	},
+	{
+		path: '/*',
+		element: <Home />,
+	},
+]);
 
 function App() {
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	useEffect(() => {
-		const user = fetchUser();
+	// useEffect(() => {
+	// 	const user = fetchUser();
 
-		if (!user) navigate('/login');
-	}, []);
+	// 	if (!user) navigate('/login');
+	// }, []);
 	return (
-		<Routes>
-			<Route path="login" element={<Login />} />
-			<Route path="/*" element={<Home />} />
-		</Routes>
-		// <RouterProvider router={router} />
+		// <Routes>
+		// 	<Route path="login" element={<Login />} />
+		// 	<Route path="/*" element={<Home />} />
+		// </Routes>
+		<RouterProvider router={router} />
 	);
 }
 
